871d28255009c01ec87bf4dd814ed7d69072217c
thanmay-bharadwaj <jsthanmaybharadwaj11@gmail.com>
2021-10-01 19:09:44 +0530

    initial commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// FAQ SECTION
+
+const faqs = document.querySelectorAll(".faq");
+
+faqs.forEach((faq) => {
+	faq.addEventListener("click", () => {
+		faq.classList.toggle("active");
+	});
+});
+
+//NAVBAR
